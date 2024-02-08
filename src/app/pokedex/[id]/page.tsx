918851ea90a7eef2f6283617f9212accf42e634a
anthondevg <robwert7991@gmail.com>
2024-02-07 20:00:07 -0400

    pokemon cards next/previous

--- src/app/pokedex/[id]/page.tsx
+++ src/app/pokedex/[id]/page.tsx
@@ -10,6 +10,7 @@ import Sprite from "../components/Sprite";
 import PokeType from "@/components/PokeType";
 import { getSprite } from "@/utilities/formatters";
 import { motion, useMotionValue, useMotionValueEvent } from "framer-motion";
+import PokemonCard from "@/components/Card/PokemonCard";
 
 export default function Page() {
   const params = useParams();
@@ -35,45 +36,54 @@ export default function Page() {
   if (isFetching) return <div className="text-2xl "></div>;
 
   return (
-    <main className="overflow-hidden bg-gradient-to-b gap-6 grow items-center m-auto relative h-screen">
+    <main className="bg-gradient-to-b overflow-x-hidden pb-6 gap-6 grow items-center m-auto relative">
       <section className="flex flex-col justify-center m-auto pt-12 items-center">
+        <PokemonCard
+          pokemonUrl={`https://pokeapi.co/api/v2/pokemon/${pokemon.id - 1}`}
+          className="absolute top-2 left-2 w-60"
+        />
+
+        <div className="absolute mx-auto w-60 z-30 h-52 bg-black"></div>
+
+        <PokemonCard
+          pokemonUrl={`https://pokeapi.co/api/v2/pokemon/${pokemon.id + 1}`}
+          className="absolute top-2 right-2 w-60"
+        />
         <motion.div
           animate={{ y: 10 }}
           whileHover={{ y: -2, shadow: "20px 10px 2px rgba(0, 0, 0, 1)" }}
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           whileTap={{ scale: 0.9 }}
+          className="z-50"
         >
           <h3
             className={`text-[#CFCFCF] z-50 text-7xl lg:text-9xl  capitalize font-black `}
           >
             {pokemon.name}
           </h3>
-        </motion.div>
-        <h3
-          className={` text-[#CFCFCF]/30 text-7xl lg:text-9xl -mb-28 lg:-mb-72 capitalize font-black `}
-        >
-          {pokemon.name}
-        </h3>
+          <h3
+            className={` text-[#CFCFCF]/30 text-7xl lg:text-9xl -mb-28 lg:-mb-72 capitalize font-black `}
+          >
+            {pokemon.name}
+          </h3>
 
-        <img
-          src={pokemon.sprites && getSprite(pokemon)}
-          className="w-72  z-50"
-        />
+          <img src={pokemon.sprites && getSprite(pokemon)} className="w-72  " />
+        </motion.div>
       </section>
 
       <div className="max-w-4xl m-auto">
         <motion.div
-          className="bg-slate-800 border-4 -mt-20 px-8 space-y-5 lg:px-16 pt-16 rounded-xl p-4 cursor-pointer mx-4 text-black relative"
+          className="  px-8 space-y-5 lg:px-16 pt-16 rounded-xl p-4 cursor-pointer mx-4 text-black relative"
           drag="x"
           dragConstraints={{ left: 0, right: 0 }}
           style={{ x }}
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
         >
-          <h2 className="text-sm text-slate-300  font-bold absolute top-4 right-4">
+          {/* <h2 className="text-sm text-slate-300  font-bold absolute top-4 right-4">
             # 000{pokemon.id}
-          </h2>
+          </h2> */}
           <h2 className="text-2xl text-slate-500  font-bold">Stats</h2>
           <section className="grid-cols-1 gap-2 lg:grid-cols-2 lg:gap-4 grid">
             {pokemon.stats &&
@@ -98,7 +108,7 @@ export default function Page() {
           </div>
           <h2 className="text-2xl text-slate-500  font-bold mb-4">Sprites</h2>
           {pokemon.sprites && (
-            <div className="flex">
+            <div className="grid grid-cols-2 lg:grid-cols-4">
               <Sprite sprite={pokemon.sprites.front_default} />
               <Sprite sprite={pokemon.sprites.back_default} />
               <Sprite sprite={pokemon.sprites.front_shiny} />
